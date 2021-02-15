5bbe35fe6035ecc923e285d75310899e55b1f403
wangpeng <wtz1998@icloud.com>
2021-02-15 14:08:45 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 王鹏 on 2021/2/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by 王鹏 on 2021/2/15.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
